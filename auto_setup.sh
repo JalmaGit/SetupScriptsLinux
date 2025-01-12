24119c53431b7a2f36677af685c158f858b5300e
Jalma <you@example.com>
2025-01-12 01:26:28 +0100

    Added Scripts

--- auto_setup.sh
+++ auto_setup.sh
@@ -22,6 +22,23 @@ sudo apt update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 2
 
 echo "______CMake Install_______"
 
+sudo apt-get update
+sudo apt-get install ca-certificates gpg wget
+
+test -f /usr/share/doc/kitware-archive-keyring/copyright ||
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ noble main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+sudo apt-get update
+
+test -f /usr/share/doc/kitware-archive-keyring/copyright ||
+sudo rm /usr/share/keyrings/kitware-archive-keyring.gpg
+
+sudo apt-get install kitware-archive-keyring
+
+yes | sudo apt-get install cmake
+
+
 
 echo "______Pycharm Setup_______"
 
